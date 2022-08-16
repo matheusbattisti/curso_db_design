01822f2a2970ada607dff1f18dbc6b5e0a70dbce
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-08-16 09:27:04 -0300

    add codigo pizzaria

--- 7_bonus_projeto_pizzaria/projeto/index.php
+++ 7_bonus_projeto_pizzaria/projeto/index.php
@@ -1,12 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Faça seu Pedido!</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+  include_once("templates/header.php");
+  include_once("process/pizza.php");
+?>
+  <div id="main-banner">
+    <h1>Faça seu Pedido</h1>
+  </div>
+  <div id="main-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2>Monte a pizza como desejar:</h2>
+          <form action="process/pizza.php" method="POST" id="pizza-form">
+            <div class="form-group">
+              <label for="borda">Borda:</label>
+              <select name="borda" id="borda" class="form-control">
+                <option value="">Selecione a borda</option>
+                <?php foreach($bordas as $borda): ?>
+                  <option value="<?= $borda["id"] ?>"><?= $borda["tipo"] ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="massa">Massa:</label>
+              <select name="massa" id="massa" class="form-control">
+                <option value="">Selecione a massa</option>
+                <?php foreach($massas as $massa): ?>
+                  <option value="<?= $massa["id"] ?>"><?= $massa["tipo"] ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="sabores">Sabores: (Máximo 3)</label>
+              <select multiple name="sabores[]" id="sabores" class="form-control">
+                <?php foreach($sabores as $sabor): ?>
+                  <option value="<?= $sabor["id"] ?>"><?= $sabor["nome"] ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+            <div class="form-group">
+              <input type="submit" class="btn btn-primary" value="Fazer Pedido!">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php
+  include_once("templates/footer.php");
+?>
